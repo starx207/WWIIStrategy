0e4b239010ac324c7c7e4f9b6aec99b17c265945
Andrew Starr <starx207@hotmail.com>
2025-09-20 22:28:38 -0400

    Add ngsx and open layers for state management and the interactive map

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,12 +1,28 @@
-import { ApplicationConfig, provideBrowserGlobalErrorListeners, provideZonelessChangeDetection } from '@angular/core';
+import {
+  ApplicationConfig,
+  provideBrowserGlobalErrorListeners,
+  provideZonelessChangeDetection,
+} from '@angular/core';
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
+import { withNgxsReduxDevtoolsPlugin } from '@ngxs/devtools-plugin';
+import { withNgxsRouterPlugin } from '@ngxs/router-plugin';
+import { withNgxsStoragePlugin } from '@ngxs/storage-plugin';
+import { provideStore } from '@ngxs/store';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
     provideZonelessChangeDetection(),
-    provideRouter(routes)
-  ]
+    provideRouter(routes),
+    provideStore(
+      [],
+      withNgxsReduxDevtoolsPlugin(),
+      withNgxsRouterPlugin(),
+      withNgxsStoragePlugin({
+        keys: '*',
+      })
+    ),
+  ],
 };
