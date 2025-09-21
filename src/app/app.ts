a7d227d1098dfe065a7d7d7f4092f79c15d048e4
Andrew Starr <starx207@hotmail.com>
2025-09-20 22:26:51 -0400

    Update app prefix and remove tests

--- src/app/app.ts
+++ src/app/app.ts
@@ -2,10 +2,10 @@ import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
-  selector: 'app-root',
+  selector: 'ww2-root',
   imports: [RouterOutlet],
   templateUrl: './app.html',
-  styleUrl: './app.scss'
+  styleUrl: './app.scss',
 })
 export class App {
   protected readonly title = signal('WWIIStrategy');
