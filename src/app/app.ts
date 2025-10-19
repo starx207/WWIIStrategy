f06c8f19088889e90433b2a50e474ba6d15a18c7
Andrew Starr <starx207@hotmail.com>
2025-10-19 10:15:01 -0400

    Add battle board to app for testing

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,9 +1,10 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { BattleBoard } from './combat/battle-board/battle-board';
 
 @Component({
   selector: 'ww2-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, BattleBoard],
   templateUrl: './app.html',
   styleUrl: './app.scss',
 })
