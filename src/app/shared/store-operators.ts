9dc1d0add1e40266d2972a4608084f4d09d15872
Andrew Starr <Andrew.Starr@civicplus.com>
2026-05-10 22:11:28 -0400

    Formatting

--- src/app/shared/store-operators.ts
+++ src/app/shared/store-operators.ts
@@ -1,4 +1,4 @@
-import { compose, removeItem, StateOperator } from "@ngxs/store/operators";
+import { compose, removeItem, StateOperator } from '@ngxs/store/operators';
 
 export function removeAll<T>(items: T[]): StateOperator<T[]> {
   const removers = items.map((i) => removeItem<T>((x) => x === i));
